5cea6163030b42f84d939d57cfd7295f2364c6a3
unknown <sadiksourov11@gmail.com>
2025-09-07 19:39:39 +0600

    footer bug resolve

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 import { Outlet } from "react-router";
-import Footer from "../footer/footer";
+import Footer from "../footer/Footer"
+
 
 
 
